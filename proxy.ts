5f5ffc37c3e4a0cf1a1c49218231b30ace4f4499
refaat1297 <a.refaat1297@gmail.com>
2026-09-25 01:19:22 +0300

    Update proxy logic to use redirect instead of rewrite and refine matcher patterns

--- proxy.ts
+++ proxy.ts
@@ -7,20 +7,19 @@ export function proxy(request: NextRequest) {
   const url = request.nextUrl
   const pathname = url.pathname
 
-  console.log('host', host)
   const isAdminHost = host.startsWith("admin-wazifa-app.vercel.app")
   if (isAdminHost) {
-    return NextResponse.rewrite(new URL("/dashboard", request.url))
+    return NextResponse.redirect(new URL("/dashboard", request.url))
   }
 
   const isMainDomain = host.startsWith("wazifa-app.vercel.app")
   if (isMainDomain && pathname.includes("/dashboard")) {
-    return NextResponse.rewrite(new URL("/", request.url))
+    return NextResponse.redirect(new URL("/", request.url))
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/jobs/:path*"],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)',],
 }
