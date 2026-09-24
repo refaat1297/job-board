5f0ae0a0e2fb595fabaf2c12c8d36a8006e416cc
refaat1297 <a.refaat1297@gmail.com>
2026-09-25 01:44:48 +0300

    Refine matcher pattern to exclude 'favicon.ico' from proxy configuration

--- proxy.ts
+++ proxy.ts
@@ -21,5 +21,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api/|_next/static|_next/image|sw\\.js).*)',],
+  matcher: ['/((?!api/|_next/static|_next/image|sw\\.js|favicon\.ico).*)',],
 }
