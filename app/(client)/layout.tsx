b250020bc37d5d310a597a64236a5acf3099d552
refaat1297 <a.refaat1297@gmail.com>
2026-09-26 00:34:55 +0300

    Restructure app layout: Add NavbarHeader, NavbarFooter components, and move routes under client segment

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,14 @@
+import NavbarHeader from "@/components/navbar/NavbarHeader";
+import NavbarFooter from "@/components/navbar/NavbarFooter";
+
+function AppLayout ({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen bg-background">
+      <NavbarHeader />
+      {children}
+      <NavbarFooter />
+    </div>
+  )
+}
+
+export default AppLayout
